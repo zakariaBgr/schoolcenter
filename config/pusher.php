9067810bd0f63225f11a890d8cf5b0cbb03c9376
Mickaël Fosso <jazzkeats@gmail.com>
2017-08-07 22:18:17 +0200

    ajout du chat

---
+++ config/pusher.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of Laravel Pusher.
+ *
+ * (c) Vincent Klaiber <hello@vinkla.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Connection Name
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the connections below you wish to use as
+    | your default connection for all work. Of course, you may use many
+    | connections at once using the manager class.
+    |
+    */
+
+    'default' => 'main',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pusher Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the connections setup for your application. Example
+    | configuration has been included, but you may add as many connections as
+    | you would like.
+    |
+    */
+
+    'connections' => [
+
+        'main' => [
+           'auth_key' => '6be04a33b08d653ffd6e',
+            'secret' => '7ae4cb69703e8d1b8919',
+            'app_id' => '379543',
+            'options' => [],
+            'host' => null,
+            'port' => null,
+            'timeout' => null,
+        ],
+
+        'alternative' => [
+            'auth_key' => 'your-auth-key',
+            'secret' => 'your-secret',
+            'app_id' => 'your-app-id',
+            'options' => [],
+            'host' => null,
+            'port' => null,
+            'timeout' => null,
+        ],
+
+    ],
+
+];
